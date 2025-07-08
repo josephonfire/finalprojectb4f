dac02704454b474e2583521f9627eff5632a1eac
Jose Pedro <josephpedroferreira@gmail.com>
2025-07-08 13:07:06 +0100

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,22 @@ import './App.css';
 import CardSearch from './components/CardSearch';
 import ChatBot from './components/ChatBot';
 import Login from './components/Login';
+import Home from './pages/Home';
+import LoginPage from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <CardSearch/>
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <div className="App">
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import CardSearch from "../components/CardSearch"
+
+function Home() {
+    return (
+     <div className="App">
+        <CardSearch/>
+      </div>
+    )
+}
+
+export default Home;
