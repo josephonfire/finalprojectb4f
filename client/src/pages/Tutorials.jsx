627c3475dede548ba00a3841fea004d692b65534
Ellie-Yeah <elisabeteff.69@gmail.com>
2025-07-09 12:06:52 +0100

    add Tutorials ao App.jsx

--- client/src/pages/Tutorials.jsx
+++ client/src/pages/Tutorials.jsx
@@ -3,3 +3,14 @@
 //caixa para alojar a lista
 //scrollbar para a lista
 
+function tutorials() {
+    return (
+        <><div>
+        < h1 > teste</h1 >
+
+        </div>
+        </>
+    )
+}
+
+export default tutorials;
