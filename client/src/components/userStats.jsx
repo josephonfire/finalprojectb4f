af160b7b272a601b12ab4efcebc535050a2c3fd4
Jose Pedro <josephpedroferreira@gmail.com>
2025-07-14 12:04:09 +0100

    Added user stats page and stats api

--- client/src/components/userStats.jsx
+++
@@ -1,9 +0,0 @@
-export async function getUserStats(token) {
-  const res = await fetch('http://localhost:3030/api/user-stats', {
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }
-  });
-  if (!res.ok) throw new Error('Erro ao buscar estatísticas');
-  return res.json();
-}
--- server/api/data/stats.js
+++ server/api/data/stats.js
@@ -25,4 +25,15 @@ async function getStatsForUser(user) {
   return { colorData, typeData, topCards };
 }
 
+export async function getUserStats(token) {
+  const res = await fetch('http://localhost:3030/api/user-stats', {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  if (!res.ok) throw new Error('Erro ao buscar estatísticas');
+  return res.json();
+}
+
+
 module.exports = { getStatsForUser };
